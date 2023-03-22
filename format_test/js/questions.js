c9625b3e5dacae1d056b6c406017f55258747c4a
Kyryl <kieremieiev@outlook.com>
2023-03-22 10:42:03 +0100

    add beweringen, add forward button functionality

---
+++ format_test/js/questions.js
@@ -0,0 +1,4 @@
+const questions = [
+    "Ik droom ervan zo goed te zijn in wat ik doe dat er voortdurend om mijn deskundige advies wordt gevraagd.",
+    ""
+]
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,56 @@
-console.log("ik werk");
+class App{
+    jsonFile
+    htmlElementTxt
+    htmlElementBtn
+    constructor(jsonFile, htmlElementTxt, htmlElementBtn){
+        this.jsonFile = jsonFile;
+        this.htmlElementTxt = htmlElementTxt;
+        this.htmlElementBtn = htmlElementBtn;
+        this.readJson();
+    }
+
+    readJson(){
+        fetch(this.jsonFile).then(
+            (responce) => {
+            return responce.json()
+        }).then((data) =>{
+            this.data = data
+            this.ObjectCreator()
+        });
+    }
+
+    ObjectCreator(){
+  
+        const transition = new Transition(this.htmlElementTxt, this.htmlElementBtn, this.data);
+    }
+}
+
+class Transition{
+    htmlElementTxt;    
+    htmlElementBtn;
+    data;
+    currentPage;
+    constructor(htmlElementTxt, htmlElementBtn, data){
+        this.htmlElementTxt = htmlElementTxt;
+        this.htmlElementBtn = htmlElementBtn;
+        this.data = data;
+        this.currentPage = 0
+
+        this.htmlElementBtn.onclick = this.ButtonClickedForward;
+    }
+
+    ButtonClickedForward = () =>{
+        console.log(this.data);
+        this.currentPage += 1;
+        this.Render();
+    }
+
+    Render(){
+        this.htmlElementTxt.innerText = this.data[0].question[this.currentPage];        
+    }
+    
+
+}
+
+
+let app = new App("/public/js/loopbaanAnkers.json", document.getElementById("js--txt"), document.getElementById("js--btn"))
