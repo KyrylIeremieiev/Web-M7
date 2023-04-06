918bd1d9e45feac64b0c96ce7b106caa37307f89
Kyryl <kieremieiev@outlook.com>
2023-04-06 16:18:58 +0200

    remove most html from vlba.html, add html generation in js. Need to convert the info page, for now the header breaks at the info page. Should be fixed after info page is converted

--- public/js/main.js
+++ public/js/main.js
@@ -29,49 +29,25 @@ class App{
     }
 
     HtmlCreator(){
-        this.htmlScoreButtonArr = []
-        this.htmlScoreButtonsWrapArr = []
-        //Breekt de hele website, wil niet meet laden
-          for(let i = 0; i < 40; i++){
-            this.htmlScoreButtonsWrap = document.createElement("ul");
-            this.htmlScoreButtonsWrap.classList.add("vragen__scoreButtonList");
-
-                for(let x = 0; x < 7; x++){
-                    this.htmlScoreButton = document.createElement("input");
-                    this.htmlScoreButton.setAttribute("type", "radio");
-                    this.htmlScoreButton.setAttribute("name", "InputButtonRadio"+i);
-                    if(x < 6){
-                        this.htmlScoreButton.setAttribute("value", x+1)
-                    }
-                    else{
-                        this.htmlScoreButton.setAttribute("value", 10)
-                    }
-                    this.htmlScoreButton.classList.add("vragen__scoreButton");
-                    this.htmlScoreButton.classList.add("js--scoreButtons");
-
-                    this.htmlScoreLi = document.createElement("li");
-                    this.htmlScoreLi.classList.add("vragen__scoreButtonListItem");
-                    this.htmlScoreLi.appendChild(this.htmlScoreButton);
-
-                    //makes array of all buttons
-                    this.htmlScoreButtonArr.push(this.htmlScoreButton);
-                    this.htmlScoreButtonsWrap.appendChild(this.htmlScoreLi);
-                }
-
-           
-           this.htmlScoreButtonsWrapArr.push(this.htmlScoreButtonsWrap);
-
-           document.getElementById("js--receiver").appendChild(this.htmlScoreButtonsWrap);
-
-        }  
+        this.html = new CreateHtml();
+        this.buttons = new CreateButtons();
     }
 
     ObjectCreator(){
-        this.valueCounter = new ValueCounter(this.data,this.htmlScoreButtonArr, this.htmlScoreButtonsWrapArr);
-        this.transition = new Transition(this.htmlNomering, this.htmlElementTxt, this.htmlElementForward, 
-        this.htmlElementBackward, this.htmlScoreButtonsWrapArr, this.data, this.progressBar);
+        this.valueCounter = new ValueCounter(this.data,this.buttons.htmlScoreButtonArr, this.buttons.htmlScoreButtonsWrapArr);
+
+        this.transition = new Transition(this.html.questions.VraagNummer, this.html.questions.VraagBewering, 
+        this.html.questions.navButtonForward, this.html.questions.navButtonBackward, 
+        this.buttons.htmlScoreButtonsWrapArr, this.data, this.html.questions.progressBar);
+
+        /*this works because the html is already created, if you were to use document.getElementById() before initializing app,
+        the variable will be the element with that id as of time. Meaning that it selects the nothing, because the element with id is not created yet.
+        This is done after init, it can be used because the element selection is done after the element is created.*/
+        this.tie = new TieTogether(document.getElementById("js--infoSection"), document.getElementById("js--vragen"), 
+        document.getElementById("js--resultSection"), document.getElementById("js--startBtn"));
     }
 }
+
 
 class Transition{
     htmlNomering;
@@ -118,7 +94,7 @@ class Transition{
             app.valueCounter.AssignValue(this.currentPage);
         }
         else{
-            tie.GoToEnd();
+            app.tie.GoToEnd();
         }
     }
 
@@ -291,6 +267,7 @@ class ValueCounter{
     }
 }
 
+
 class Charts{
     constructor(tf, am, au, ze, oc, dv, ui, ls){
         this.TF = tf;
@@ -338,6 +315,8 @@ class Charts{
         });
     }
 }
+
+
 class TieTogether{
     htmlStartSection
     htmlFormSection
@@ -367,8 +346,326 @@ class TieTogether{
         this.htmlResultSection.style.display = "flex";
     }
 }
-let tie = new TieTogether(document.getElementById("js--infoSection"), document.getElementById("js--vragen"), 
-document.getElementById("js--resultSection"), document.getElementById("js--startBtn"));
+
+class CreateButtons{
+    constructor(){
+        this.htmlScoreButtonArr = []
+        this.htmlScoreButtonsWrapArr = []
+        //Breekt de hele website, wil niet meet laden
+          for(let i = 0; i < 40; i++){
+            this.htmlScoreButtonsWrap = document.createElement("ul");
+            this.htmlScoreButtonsWrap.classList.add("vragen__scoreButtonList");
+
+                for(let x = 0; x < 7; x++){
+                    this.htmlScoreButton = document.createElement("input");
+                    this.htmlScoreButton.setAttribute("type", "radio");
+                    this.htmlScoreButton.setAttribute("name", "InputButtonRadio"+i);
+                    if(x < 6){
+                        this.htmlScoreButton.setAttribute("value", x+1)
+                    }
+                    else{
+                        this.htmlScoreButton.setAttribute("value", 10)
+                    }
+                    this.htmlScoreButton.classList.add("vragen__scoreButton");
+                    this.htmlScoreButton.classList.add("js--scoreButtons");
+
+                    this.htmlScoreLi = document.createElement("li");
+                    this.htmlScoreLi.classList.add("vragen__scoreButtonListItem");
+                    this.htmlScoreLi.appendChild(this.htmlScoreButton);
+
+                    //makes array of all buttons
+                    this.htmlScoreButtonArr.push(this.htmlScoreButton);
+                    this.htmlScoreButtonsWrap.appendChild(this.htmlScoreLi);
+                }
+
+           
+           this.htmlScoreButtonsWrapArr.push(this.htmlScoreButtonsWrap);
+
+           document.getElementById("js--receiver").appendChild(this.htmlScoreButtonsWrap);
+
+        }  
+    }
+}
+
+class CreateHtml{
+    constructor(){
+        this.header = new CreateHeader();
+        this.info = new CreateInfo();
+        this.questions = new CreateQuestion();
+    }
+}
+
+
+class CreateHeader{
+    constructor(){
+        this.createHeader()
+    }
+
+
+    // How it looks in Html
+    /*  <header class="banner">
+            <figure class="banner__logo">
+                <img src="img/site-logo-2.png" alt="" class="banner__logoImage">
+            </figure>
+        </header> */
+
+
+    createHeader(){
+        this.HeaderLogo = document.createElement("img");
+        this.HeaderLogo.setAttribute("src", "img/site-logo-2.png");
+        this.HeaderLogo.setAttribute("alt", "De logo van Doesburg-Coaching");
+        this.HeaderLogo.setAttribute("class", "banner__logoImage");
+
+        this.HeaderFigure = document.createElement("figure");
+        this.HeaderFigure.setAttribute("class", "banner__logo");
+        
+        //Figure appends Logo
+        this.HeaderFigure.appendChild(this.HeaderLogo);
+
+        this.HeaderBanner = document.createElement("header");
+        this.HeaderBanner.setAttribute("class","banner")
+        
+        //Header appends Figure, body appends header
+        this.HeaderBanner.appendChild(this.HeaderFigure);
+        document.getElementsByTagName("body")[0].appendChild(this.HeaderBanner);
+    }
+}
+
+class CreateInfo{
+    constructor(){
+        this.createInfo();
+    }
+
+/*  <section class="info" id="js--infoSection">
+        <article class="info__textWrapper">
+            <h2 class="info__text">
+                Je zal zomenteen de Vragenlijst van Loopbaanankers (Schein)
+                maken waaruit je zal zien welke loopbaananker het beste bij jou past Start hieronder de test
+            </h2>
+            <h2 class="info__text">
+                Hier komt informatie te staan over de test Hier komt informatie te
+                staan over de test Hier komt informatie te staan over de test
+            </h2>
+            <h2 class="info__text">
+                Hier komt informatie te staan over de test Hier komt informatie te
+                staan over de test Hier komt informatie te staan over de test
+            </h2>
+        </article>
+        <button class="info__startButton" id="js--startBtn">Start de Test</button>
+    </section>  */
+
+    createInfo(){
+        console.log("yep")
+        this.FirstInfoText = document.createElement("h2");
+        this.FirstInfoText.setAttribute("class", "info__text");
+        this.FirstInfoText.innerText = "Je zal zomenteen de Vragenlijst van Loopbaanankers (Schein) maken waaruit je zal zien welke loopbaananker het beste bij jou past Start hieronder de test"
+
+        this.SecondInfoText = document.createElement("h2");
+        this.SecondInfoText.setAttribute("class", "info__text");
+        this.SecondInfoText.innerText = "Hier komt informatie te staan over de test Hier komt informatie te staan over de test Hier komt informatie te staan over de test"
+        
+        this.ThirdInfoText = document.createElement("h2");
+        this.ThirdInfoText.setAttribute("class", "info__text");
+        this.ThirdInfoText.innerText = "Hier komt informatie te staan over de test Hier komt informatie te staan over de test Hier komt informatie te staan over de test"
+
+        this.InfoArticle = document.createElement("article");
+        this.InfoArticle.setAttribute("class", "info__textWrapper");
+
+        //info article appends first, second and third info text
+        this.InfoArticle.appendChild(this.FirstInfoText);
+        this.InfoArticle.appendChild(this.SecondInfoText);
+        this.InfoArticle.appendChild(this.ThirdInfoText);
+
+        this.InfoButton = document.createElement("button");
+        this.InfoButton.setAttribute("class", "info__startButton")
+        this.InfoButton.setAttribute("id", "js--startBtn")
+        this.InfoButton.innerText = "Start de test!"
+
+        this.InfoSection = document.createElement("section");
+        this.InfoSection.setAttribute("class", "info")
+        this.InfoSection.setAttribute("id", "js--infoSection")
+
+        //Section append article and button
+        this.InfoSection.appendChild(this.InfoArticle);
+        this.InfoSection.appendChild(this.InfoButton);
+
+        //Body append Section
+        document.getElementsByTagName("body")[0].appendChild(this.InfoSection);
+    }
+}
+
+class CreateQuestion{
+    constructor(){
+        this.createQuestion();
+    }
+    
+    //this is how the html should look like
+  /*<section class="vragen" id="js--vragen">
+        <div class="vragen__vraagNummerWrapper">
+            <p class="vragen__vraagNummer" id="js--beweringNomering">Vraag 1:</p>
+        </div>
+
+        <h1 class="vragen__vraag" id="js--bewering">Ik droom ervan zo goed te zijn in wat ik doe dat er voortdurend
+            om mijn deskundige advies wordt gevraagd
+        </h1>
+
+        <div class="vragen__labelWrapper">
+            <label class="vragen__label">Niet zo zeer</label>
+            <label class="vragen__label">Heel erg</label>
+        </div>
+
+        <ul class="vragen__scoreList">
+            <li class="vragen__scoreListItem">
+                <label for="InputFirstRadio" class="vragen__scoreListP">1</label>
+            </li>
+            <li class="vragen__scoreListItem">
+                <label for="InputSecondRadio" class="vragen__scoreListP">2</label>
+            </li>
+            <li class="vragen__scoreListItem">
+                <label for="InputThirdRadio" class="vragen__scoreListP">3</label>
+            </li>
+            <li class="vragen__scoreListItem">
+                <label for="InputForthRadio" class="vragen__scoreListP">4</label>
+            </li>
+            <li class="vragen__scoreListItem">
+                <label for="InputFifthRadio" class="vragen__scoreListP">5</label>
+            </li>
+            <li class="vragen__scoreListItem">
+                <label for="InputSixthRadio" class="vragen__scoreListP">6</label>
+            </li>
+            <li class="vragen__scoreListItem" id="js--tenthBtnLi">
+                <label for="InputTenthRadio" class="vragen__scoreListP" id="js--tenthBtnLabel">10</label>
+            </li>
+        </ul>
+
+        <div id="js--receiver" style="height: 100%; width: 100%;"></div>
+
+        <div class="vragen__navButtons">
+            <button class="vragen__navButton" id="js--backward"> &lt; </button>
+            <button class="vragen__navButton" id="js--forward"> &gt; </button>
+        </div>
+
+        <div class="vragen__progBar">
+            <div class="vragen__progBarFill" id="js--progressBar"></div>
+        </div>
+
+        <p class="vragen__info">Je mag op 3 vragen die van jou het meest van toepassing zijn 10 punten geven</p>
+    </section> */
+
+    createQuestion(){
+        this.VraagNummer = document.createElement("p");
+        this.VraagNummer.setAttribute("class", "vragen__vraagNummer");
+        this.VraagNummer.setAttribute("id", "js--beweringNomering")
+        this.VraagNummer.innerText = "Vraag: 1"
+
+        this.VraagNummerWrapper = document.createElement("div");
+        this.VraagNummerWrapper.setAttribute("class", "vragen__vraagNummerWrapper");
+
+        //Div append p
+        this.VraagNummerWrapper.appendChild(this.VraagNummer);
+
+        //this is stand alone element who doesnt have a child and whos only parent is the section
+        this.VraagBewering = document.createElement("h1");
+        this.VraagBewering.setAttribute("class", "vragen__vraag");
+        this.VraagBewering.setAttribute("id", "js--bewering");
+        this.VraagBewering.innerText = "Ik droom ervan zo goed te zijn in wat ik doe dat er voortdurend om mijn deskundige advies wordt gevraagd";
+
+        this.VraagNietZeerLabel = document.createElement("label");
+        this.VraagNietZeerLabel.setAttribute("class", "vragen__label");
+        this.VraagNietZeerLabel.innerText = "Niet zo zeer";
+
+        this.VraagErgZeerLabel = document.createElement("label");
+        this.VraagErgZeerLabel.setAttribute("class", "vragen__label");
+        this.VraagErgZeerLabel.innerText = "Heel erg";
+
+        this.VraagLabelWrapper = document.createElement("div");
+        this.VraagLabelWrapper.setAttribute("class", "vragen__labelWrapper")
+
+        //div append label
+        this.VraagLabelWrapper.appendChild(this.VraagNietZeerLabel);
+        this.VraagLabelWrapper.appendChild(this.VraagErgZeerLabel);
+
+        //create li(label)
+        this.VraagScoreList = document.createElement("ul");
+        this.VraagScoreList.setAttribute("class", "vragen__scoreList");
+
+        for(let i = 0; i < 6; i++){
+            this.VraagLabel = document.createElement("label");
+            this.VraagLabel.setAttribute("class", "vragen__scoreListP");
+            this.VraagLabel.innerText = i+1;
+            this.VraagLi = document.createElement("li");
+            this.VraagLi.setAttribute("class", "vragen__scoreListItem");
+            this.VraagLi.appendChild(this.VraagLabel);
+            
+            this.VraagScoreList.appendChild(this.VraagLi)
+        }
+
+        this.VraagTenLabel = document.createElement("label");
+        this.VraagTenLabel.setAttribute("class", "vragen__scoreListP");
+        this.VraagTenLabel.setAttribute("id", "js--tenthBtnLabel")
+        this.VraagTenLabel.innerText = "10"
+        this.VraagTenLi = document.createElement("li");
+        this.VraagTenLi.setAttribute("class", "vragen__scoreListItem");
+        this.VraagTenLi.setAttribute("id", "js--tenthBtnLi")
+        this.VraagTenLi.appendChild(this.VraagTenLabel);
+
+        this.VraagScoreList.appendChild(this.VraagTenLi);
+
+        //receiver
+        this.receiver = document.createElement("div");
+        this.receiver.setAttribute("id", "js--receiver");
+        this.receiver.style.width = "100%";
+        this.receiver.style.height = "100%";
+
+        //forward backward buttons
+        this.navButtonBackward = document.createElement("button");
+        this.navButtonBackward.setAttribute("class", "vragen__navButton");
+        this.navButtonBackward.setAttribute("id", "js--backward")
+        this.navButtonBackward.innerText = "<"
+
+        this.navButtonForward = document.createElement("button");
+        this.navButtonForward.setAttribute("class", "vragen__navButton");
+        this.navButtonForward.setAttribute("id", "js--forward");
+        this.navButtonForward.innerText = ">"
+
+        this.navButtons = document.createElement("div");
+        this.navButtons.setAttribute("class", "vragen__navButtons");
+        this.navButtons.appendChild(this.navButtonBackward);
+        this.navButtons.appendChild(this.navButtonForward);
+
+        //progressbar
+        this.progressBar = document.createElement("div");
+        this.progressBar.setAttribute("class", "vragen__progBarFill");
+        this.progressBar.setAttribute("id", "js--progressBar");
+
+        this.progressBarWrap = document.createElement("div");
+        this.progressBarWrap.setAttribute("class", "vragen__progBar");
+        //progress bar wrap append progress bar
+        this.progressBarWrap.appendChild(this.progressBar)
+
+        //info
+        this.VraagInfo = document.createElement("p");
+        this.VraagInfo.setAttribute("class", "vragen__info")
+        this.VraagInfo.innerText = "Je mag op 3 vragen die van jou het meest van toepassing zijn 10 punten geven";
+
+        this.VraagSection = document.createElement("section");
+        this.VraagSection.setAttribute("class", "vragen")
+        this.VraagSection.setAttribute("id", "js--vragen")
+
+        //vragen section append everything above
+        this.VraagSection.appendChild(this.VraagNummerWrapper);
+        this.VraagSection.appendChild(this.VraagBewering);
+        this.VraagSection.appendChild(this.VraagLabelWrapper);
+        this.VraagSection.appendChild(this.VraagScoreList);
+        this.VraagSection.appendChild(this.receiver);
+        this.VraagSection.appendChild(this.navButtons);
+        this.VraagSection.appendChild(this.progressBarWrap);
+        this.VraagSection.appendChild(this.VraagInfo);
+        
+        document.getElementsByTagName("body")[0].appendChild(this.VraagSection);
+    }
+
+}
 
 let app = new App("/js/loopbaanAnkers.json", document.getElementById("js--beweringNomering"), 
 document.getElementById("js--bewering"), document.getElementById("js--forward"), 
